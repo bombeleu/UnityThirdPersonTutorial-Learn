41bd5e4e595935d65c648e653215a25e85766f7e
HUANG Cheng <patrick.huangcheng@gmail.com>
2014-08-12 23:03:36 +0800

    Finished a 3rdPerson Camera

---
+++ Assets/Scripts/CharacterControllerLogic.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// #DESCRIPTION OF CLASS#
+/// </summary>
+
+public class CharacterControllerLogic : MonoBehaviour 
+{
+	#region Variables (private)
+
+	[SerializeField]
+	private Animator animator;
+	[SerializeField]
+	private float directionDampTime = .25f;
+
+	private float speed = 0.0f;
+	private float status = 0.0f;
+	private float h = 0.0f;
+	private float v = 0.0f;
+	#endregion
+
+	#region Properties (public)
+
+	#endregion
+
+	#region Unity event functions
+
+	/// <summary> 
+	/// Use this for initialization
+	/// </summary>
+	void Start () 
+	{
+		animator = GetComponent<Animator> ();
+
+		if (animator.layerCount >= 2) 
+		{
+			animator.SetLayerWeight(1,1);		
+		}
+	}
+
+	/// <summary>
+	/// Update is called once per frame
+	/// </summary>
+	void Update () 
+	{
+		if (animator) 
+		{
+			// Pull values from controller/keyboard
+			h = Input.GetAxis("Horizontal");
+			v = Input.GetAxis("Vertical");
+
+			speed = new Vector2(h, v).sqrMagnitude;
+
+			animator.SetFloat("Speed", speed);
+			animator.SetFloat("Status", h, directionDampTime, Time.deltaTime);
+			Debug.Log(speed);
+			
+
+		}
+	}
+
+	/// <summary>
+	/// Debugging information should be put here/
+	/// </summary>
+
+	void OnDrawGizmos ()
+	{
+
+
+	}
+
+	#endregion
+
+	#region Methods
+
+	#endregion Methods
+
+}
---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamera : MonoBehaviour {
+
+	#region Variables (private)
+
+	[SerializeField]
+	private float distanceAway;
+	[SerializeField]
+	private float distanceUp;
+	[SerializeField]
+	private float smooth;
+	[SerializeField]
+	private Transform follow;
+	private Vector3 targetPosition;
+
+	#endregion
+
+	#region Properties (public)
+
+	#endregion
+
+	#region Unity event functions
+
+	// Use this for initialization
+	void Start () 
+	{
+		follow = GameObject.FindWithTag ("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void LateUpdate ()
+	{
+		//setting the target position to be the correct offset from the hovercraft
+		targetPosition = follow.position + follow.up * distanceUp - follow.forward* distanceAway;
+		Debug.DrawRay (follow.position, Vector3.up * distanceUp, Color.red);
+		Debug.DrawRay (follow.position, -1f  * follow.forward * distanceAway, Color.blue);
+		Debug.DrawLine (follow.position, targetPosition, Color.magenta);
+
+		//making a smooth transition between it's current position and the position it vants to be in
+		transform.position = Vector3.Lerp (transform.position, targetPosition, Time.deltaTime * smooth);
+
+		// make sure the camera is looking the right way
+		transform.LookAt(follow);
+	}
+	#endregion
+}
